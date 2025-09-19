edbf815db546c05e20c48bb0934585e915624f06
Hilmi Razib Yusuf <hilmiyusuf197@gmail.com>
2025-09-19 11:37:11 +0700

    Add partners relationship to Service model

--- app/Models/Service.php
+++ app/Models/Service.php
@@ -6,5 +6,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class Service extends Model
 {
-    //
+    public function partners() { return $this->belongsToMany(Partner::class, 'partner_services')->withPivot(['price_cents','duration_min'])->withTimestamps(); }
 }
