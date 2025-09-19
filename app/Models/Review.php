c8a122918cb1f8d121f529eed795921ac8a535f2
Hilmi Razib Yusuf <hilmiyusuf197@gmail.com>
2025-09-19 11:38:38 +0700

    Define relationships for order, partner, and customer in Review model

--- app/Models/Review.php
+++ app/Models/Review.php
@@ -6,5 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Review extends Model
 {
-    //
+    public function order() { return $this->belongsTo(Order::class); }
+    public function partner() { return $this->belongsTo(Partner::class); }
+    public function customer() { return $this->belongsTo(User::class, 'customer_id'); }
 }
