0e944e975de4a035e4e0239f01bddcde7c4d83ed
Hilmi Razib Yusuf <hilmiyusuf197@gmail.com>
2025-09-19 11:37:45 +0700

    Define relationships and casts in Order model

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -6,5 +6,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class Order extends Model
 {
-    //
+    protected $casts = [
+        'scheduled_at' => 'datetime',
+        'placed_at'    => 'datetime',
+    ];
+
+    public function customer() { return $this->belongsTo(User::class, 'customer_id'); }
+    public function partner() { return $this->belongsTo(Partner::class); }
+    public function service() { return $this->belongsTo(Service::class); }
+    public function payment() { return $this->hasOne(Payment::class); }
+    public function review() { return $this->hasOne(Review::class); }
 }
